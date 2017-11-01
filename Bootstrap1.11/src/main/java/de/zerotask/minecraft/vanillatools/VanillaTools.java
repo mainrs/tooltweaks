dc30c189620202d2767f3ad0b16b9092ac82fd3d
Sylvain VISTE <viste.sylvain@gmail.com>
2017-11-01 14:38:49 +0100

    Adding the 1.11 version

---
+++ Bootstrap1.11/src/main/java/de/zerotask/minecraft/vanillatools/VanillaTools.java
@@ -0,0 +1,37 @@
+package de.zerotask.minecraft.vanillatools;
+
+import de.zerotask.minecraft.vanillatools.handler.BowHandler;
+import de.zerotask.minecraft.vanillatools.handler.HoeHandler;
+import de.zerotask.minecraft.vanillatools.handler.SwordHandler;
+import de.zerotask.minecraft.vanillatools.handler.ToolHandler;
+import de.zerotask.minecraft.vanillatools.reference.IReference;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+/**
+ * A simple mod used to disable all vanilla bows, hoes, swords and tools.
+ *
+ * @author Sven Lechner (SirWindfield)
+ * @version 1.0
+ */
+@Mod(modid = IReference.ID, version = IReference.VERSION,
+        dependencies = "required-after:compatlayer@[0.2.8,)",
+        useMetadata = true,
+        acceptedMinecraftVersions = "[1.10.2,1.12)")
+public class VanillaTools {
+
+    /**
+     * Registers all needed Forge event handlers.
+     *
+     * @param event The initialization event.
+     */
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new BowHandler());
+        MinecraftForge.EVENT_BUS.register(new HoeHandler());
+        MinecraftForge.EVENT_BUS.register(new SwordHandler());
+        MinecraftForge.EVENT_BUS.register(new ToolHandler());
+    }
+}
---
+++ Bootstrap1.11/src/main/java/de/zerotask/minecraft/vanillatools/reference/IReference.java
@@ -0,0 +1,14 @@
+package de.zerotask.minecraft.vanillatools.reference;
+
+/**
+ * An {@code interface} holding the mod information (used as reference).
+ *
+ * @author Sven Lechner (SirWindfield)
+ * @version 1.0
+ */
+public interface IReference {
+
+    String ID = "vanillatools";
+
+    String VERSION = "1.0.3";
+}
